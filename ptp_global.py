9f5236b79539a9fe144f8a5982aa8a295b6a9bd4
abc@autodl <abc@bowhead-tech.com>
2022-12-12 13:57:10 +0800

    update

--- ptp_global.py
+++ ptp_global.py
@@ -17,7 +17,8 @@ def init_model():
     global tokenizer
 
     device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
-    ldm_stable = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=MY_TOKEN).to(device)
+    #ldm_stable = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=MY_TOKEN).to(device)
+    ldm_stable = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", use_auth_token=MY_TOKEN).to(device)
     tokenizer = ldm_stable.tokenizer
 
     return ldm_stable
